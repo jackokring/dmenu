51517c4788e3386730b6076c07d52a24f69eef1b
jackokring <jackokring@gmail.com>
2024-07-13 20:46:22 +0000

    Saturday 2024-07-13 20:46:22

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@
 VERSION = 5.3
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX = ${HOME}
+MANPREFIX = $(PREFIX)/.local/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
